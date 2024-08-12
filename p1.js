7214c62a2f41abab4f82861aa983dbd4beeaa9e1
angelina <132501413+angelina0078@users.noreply.github.com>
2024-08-12 17:07:57 -0300

    Add files via upload

---
+++ p1.js
@@ -0,0 +1,9 @@
+//1- Cargar un arreglo con diez estaturas de personas
+
+var arregloAltura=[];
+var n=10;
+for (let i=0;i<n;i++) {
+   arregloAltura=["1,70","1,65","1,75","1,63","1,5","1,55","1,58","1,78","1,83","1,85"]
+console.log(arregloAltura)
+
+//a. Agregar una estatura en la última posición. 
---
+++ p3.js
@@ -0,0 +1,16 @@
+//5- Cargar un arreglo con las edades de una cantidad N de personas. Luego eliminar las
+//primeras cinco.
+
+var arregloEdad = [];
+let n = parseInt(prompt("ingrese cantidad de edades"))
+for (let i = 0; i < n; i++); {
+    arregloVacio[i] = Math.floor(Math.random() * 10);
+    console.log(arregloAltura);
+    arregloEdad.shift
+    arregloEdad.shift
+    arregloEdad.shift
+    arregloEdad.shift
+    arregloEdad.shift
+
+
+}
